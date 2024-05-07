166310cce59444d4dfba384154367a34d9cd331d
Veyefill <nojusklimov@gmail.com>
2024-05-07 04:45:41 +0300

    Exercise done

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [shopItem, setShopItem] = useState("");
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const Add = () => {
+    setShoppingList([...shoppingList, shopItem]);
+    setShopItem("");
+  }
+
+  const Clear = () => {
+    setShoppingList([]);
+    setShopItem("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TextInput style={{ width: 300, borderColor: 'black', borderWidth: 1 }} onChangeText={shopItem => setShopItem(shopItem)} value={shopItem}></TextInput>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={Add} title="Add"></Button>
+        <View style={{ marginLeft: 10 }}>
+          <Button onPress={Clear} title="Clear"></Button>
+        </View>
+      </View>
+      <Text style={{ fontWeight: "bold", color: "blue" }}>Shopping List</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <FlatList
+          data={shoppingList}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+              <Text>{item}</Text>
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        ></FlatList>
+      </View>
+    </View >
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
